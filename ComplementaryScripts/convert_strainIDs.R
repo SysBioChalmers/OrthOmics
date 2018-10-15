e7a756aff97ed355c9fdb6c0efdad5de5ba48f1e
Ivan Domenzain <ivand@chalmers.se>
2018-10-15 07:12:09 +0200

    feat: Added strains IDs converter script

---
+++ ComplementaryScripts/convert_strainIDs.R
@@ -0,0 +1,44 @@
+#Strains identifiers conversion: Gets a list of gene IDs for a given Y. lipolytica 
+#strain (W29 or CLIB122) or K. marxianus (DMKU or CBS 6556) and returns a new one 
+#with the other strain identifiers (limited those genes for which a single copy 
+#ortholog is available)
+
+convert_strainIDs <- function(org,strain,dataset){
+  print(org)
+  IDs <- dataset$genes
+  print(length(IDs))
+  if (all(tolower(org) == 'yli')){
+    #Load annotation data: single copy orthologs W29 <-> CLIB122
+    setwd('/Users/ivand/Documents/GitHub/CHASSY-Multi-Omics-Analyisis/Orthologs')
+    IDs_table <- read.delim('Yali_annotation.txt', header = TRUE, sep = "\t",stringsAsFactors=FALSE)
+    #Discard internal CHASSY ids column
+    IDs_table <- IDs_table[,2:3] 
+    #Standardize format
+    IDs_table$CLIB122 <- gsub('YALI0','YALI0_',IDs_table$CLIB122)
+  }
+  
+  if (all(tolower(org) == 'kma')){
+    #Load annotation data: single copy orthologs DMKU <-> CBS6556
+    setwd('/Users/ivand/Documents/GitHub/CHASSY-Multi-Omics-Analyisis/Orthologs')
+    IDs_table <- read.delim('Kma_annotation.txt', header = TRUE, sep = "\t",stringsAsFactors=FALSE)
+    #Extract columns of interest
+    IDs_table[,3] <- IDs_table[,6]
+    IDs_table <- IDs_table[,2:3]
+    colnames(IDs_table) <- c('DMKU','CBS')
+  }
+  
+  #Identify strain column and get IDs
+  column    <- match(tolower(strain), tolower(colnames(IDs_table)))
+  strainIDs <- IDs_table[,column]
+  newCol <- c(1,2)[c(1,2)!=column]
+  #Get other strain's ids
+  matches <- match(IDs,IDs_table[,column])
+  #Get indexes for converted elements
+  converted <- !is.na(matches)
+  print(sum(converted))
+  #Convert just those IDs for which a match was found
+  newIDs  <- IDs
+  newIDs[converted]  <- IDs_table[matches[converted],newCol]
+  dataset$genes <- newIDs
+  return(dataset)
+}

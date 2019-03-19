1f6214580a4661a893dbfa5f2047edb9e9517448
Ivan Domenzain <ivand@chalmers.se>
2019-03-19 13:39:36 +0100

    fix: missing values are set to zero in the loading step

--- ComplementaryScripts/loadRNAdata.R
+++ ComplementaryScripts/loadRNAdata.R
@@ -2,11 +2,12 @@ loadRNAdata <- function(dataPath,organism){
   path <- paste(dataPath,'/',organism, sep = '')
   setwd(path)
   filename <- paste(organism,'_RNA_RAW_counts.csv',sep='')
-  dataset  <-read.csv(filename,row.names = 1)
+  #dataset  <-read.csv(filename,row.names = 1)
+  dataset <- read.delim(filename, header = TRUE, sep = ",",stringsAsFactors=FALSE, na.strings = "NA")
   # Add the grouping information (and enforce the order of the samples).
   if (all(organism == 'yli')){
     conditions <- c('Ref','HiT','LpH')
-    colorValues <- c("black", "red", "#009E73")
+    colorValues <- c("grey", "red", "#009E73")
     replicates <- c(4,3,4)
     group <- factor(c(rep(conditions[1],replicates[1]),
                       rep(conditions[2],replicates[2]),
@@ -14,12 +15,12 @@ loadRNAdata <- function(dataPath,organism){
   } else {
     if (all(organism == 'cpk')){
     conditions <- c('Ref','HiT','LpH','Osm')
-    colorValues <- c("black", "red", "#009E73","blue")
+    colorValues <- c("grey", "red", "#009E73","blue")
     replicates <- c(3,3,3,3)
     }
     if (all(organism == 'kma')){replicates <- c(3,2,3,3)}
     conditions <- c('Ref','HiT','LpH','Osm')
-    colorValues <- c("black", "red", "#009E73","blue")
+    colorValues <- c("grey", "red", "#009E73","blue")
     group <- factor(c(rep(conditions[1],replicates[1]),
                       rep(conditions[2],replicates[2]),
                       rep(conditions[3],replicates[3]),
@@ -32,7 +33,11 @@ loadRNAdata <- function(dataPath,organism){
       cols <- c(cols,paste(conditions[i],'_',j,sep=''))
     }
   }
-  colnames(dataset) <- cols
+  genes                   <- dataset[,1]
+  dataset                 <- dataset[,2:ncol(dataset)]
+  colnames(dataset)       <- cols
+  dataset[is.na(dataset)] <- 0
+  
   remove(cols)
-  return(list(dataset,conditions,colorValues,replicates,group))
+  return(list(dataset,genes,conditions,colorValues,replicates,group))
 }

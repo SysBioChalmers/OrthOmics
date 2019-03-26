19d83fc8614b5fdc5ee28d5823a53ee30cfb059e
Ivan Domenzain <ivand@chalmers.se>
2019-03-26 14:37:52 +0100

    feat: added createIntegratedTable.R
    - Function that integrates RNAseq DE analysis results with absolute proteomics measurements and orthology gene groups information

---
+++ ComplementaryScripts/createIntegratedTable.R
@@ -0,0 +1,116 @@
+createIntegratedTable <- function(dataset,grouping,metric,stringent,coverage){
+#Function that integrates the results from RNAseq DE with absolute proteomics
+#measurements and genes annotation.
+#
+# Ivan Domenzain. created 2019-03-25
+#
+  
+#==================================== DEFINE VARIABLES =======================================
+#Provide organism code [sce,kma,yli]
+organism    <- 'yli'
+if (all(organism == 'yli')){
+  conditions <- c('HiT','LpH')
+  colorValues <- c("red", "#009E73")
+} else {
+  conditions <- c('HiT','LpH','Osm')
+  colorValues <- c("red", "#009E73","blue")
+}
+#Define DE thresholds
+FDR        <- 0.01
+logPval    <- abs(log10(FDR))
+log2FC     <- 1
+adjustedP  <- TRUE
+#=============================== Relevant directories ======================================
+#Relevant paths (The user should provide the path in which the repository is stored)
+repoPath    <- '/Users/ivand/Documents/GitHub/CHASSY_multiOmics_Analysis'
+#Internal functions path
+scriptsPath <- paste(repoPath,'/ComplementaryScripts',sep='')
+DBpath      <- paste(repoPath,'/Databases/Uniprot/',sep='')
+RNApath     <- paste(repoPath,'/RNA-seq',sep='')
+Protpath    <- paste(repoPath,'/Proteomics/Results/',organism,sep='')
+Orthpath    <- paste(repoPath,'/Orthologs',sep='')
+#Load absolute proteomics measurements (all conditions)
+setwd(Protpath)
+fileName <- paste(organism,'_abs_NSAF_filtered.csv',sep='')
+prot_Abs <- read.delim(fileName, header = TRUE, sep = ",",stringsAsFactors=FALSE, na.strings = "NA")
+proteins <- prot_Abs[,1]
+prot_Abs <- prot_Abs[,2:ncol(prot_Abs)]
+#Load gene groups (orthology) list
+setwd(Orthpath)
+fileName   <- paste(organism,'_orthology_groups.txt',sep='')
+geneGroups <- read.delim(fileName, header = TRUE, sep = "\t",stringsAsFactors=FALSE, na.strings = "NA")
+#Transform groups to numeric values
+if (all(organism=='kma')){
+  proteins <- gsub('KMXK','KMXK_',proteins)
+  geneGroups[(which(geneGroups[,2]=="C+C")),2] <- 5
+  geneGroups[(which(geneGroups[,2]=="K+L")),2] <- 4
+  geneGroups[(which(geneGroups[,2]=="K+S")),2] <- 3
+  geneGroups[(which(geneGroups[,2]=="K+Y")),2] <- 2
+  geneGroups[(which(geneGroups[,2]=="K+H")),2] <- 1
+} else{
+  if (all(organism=='yli')){
+    geneGroups[(which(geneGroups[,2]=="W+W")),2]  <- 5
+    geneGroups[(which(geneGroups[,2]=="Y+B")),2]  <- 4
+    geneGroups[(which(geneGroups[,2]=="Y+CH")),2] <- 3
+    geneGroups[(which(geneGroups[,2]=="Y+K")),2]  <- 2
+    geneGroups[(which(geneGroups[,2]=="Y+H")),2]  <- 1
+  } else {
+      if (all(organism=='sce')){
+        geneGroups[(which(geneGroups[,2]=="S+S")),2]  <- 5
+        geneGroups[(which(geneGroups[,2]=="S+E")),2]  <- 4
+        geneGroups[(which(geneGroups[,2]=="S+K")),2]  <- 3
+        geneGroups[(which(geneGroups[,2]=="S+Y")),2]  <- 2
+        geneGroups[(which(geneGroups[,2]=="S+H")),2]  <- 1
+      } else {
+          geneGroups[(which(geneGroups[,2]=="C+C")),2]  <- 5
+          geneGroups[(which(geneGroups[,2]=="C+E")),2]  <- 4
+          geneGroups[(which(geneGroups[,2]=="C+K")),2]  <- 3
+          geneGroups[(which(geneGroups[,2]=="C+Y")),2]  <- 2
+          geneGroups[(which(geneGroups[,2]=="C+H")),2]  <- 1
+      }
+  }
+}
+
+
+#Loop through all conditions 
+tableData <- c()
+for (i in 1:length(conditions)){
+  condition <- conditions[i]
+  #Load RNAseq DE results for the i-th condition
+  setwd(paste(RNApath,'/',organism,'/Results/DE_log2FC_',log2FC,'_FDR_',FDR,sep=''))
+  fileName    <- paste(organism,'_RNA_ref_',conditions[i],'.csv',sep='')
+  RNA_DE_data <- read.delim(fileName, header = TRUE, sep = ",",stringsAsFactors=FALSE, na.strings = "NA")
+  genes       <- RNA_DE_data$genes
+  for (j in 1:length(genes)){
+    row  <- c()
+    gene <- genes[j]
+    #Identify if j-th was significantly DE
+    significance <- FALSE
+    if (RNA_DE_data$FDR[j] < FDR & abs(RNA_DE_data$logFC[j])>=log2FC){
+        significance <- TRUE
+    }
+    direction <- sign(RNA_DE_data$logFC[j])
+    #Search protein abundances for queried gene
+    ProtIndex <- which(proteins == gene)
+    if (length(ProtIndex)>0){
+      P_ref  <- prot_Abs[ProtIndex,1]
+      P_cond <- prot_Abs[ProtIndex,i+1]
+    } else {
+      P_ref  <- NaN
+      P_cond <- NaN
+    }
+    #Search gene group
+    geneIndex <- which(geneGroups[,1] == gene)
+    if (length(geneIndex)>0){
+      group  <- geneGroups[geneIndex,2]
+    } else {
+      group  <- NaN
+    }
+    #if (significance & !is.na(P_ref)){  
+    if (significance){  
+      row <- cbind(gene,condition,significance,direction,P_ref,P_cond,group) 
+      tableData <- rbind(tableData,row)
+    } 
+    }
+}
+}

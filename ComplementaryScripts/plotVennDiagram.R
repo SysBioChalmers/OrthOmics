82d64c4fe6d7feab57b757b16c94b1a8aabfb1f7
Ivan Domenzain <ivand@chalmers.se>
2019-11-26 12:28:48 +0100

    feat: allow generation of quintuple venn diagrams

--- ComplementaryScripts/plotVennDiagram.R
+++ ComplementaryScripts/plotVennDiagram.R
@@ -4,7 +4,7 @@ library(VennDiagram)
   if (nargin < 6){scaleF = FALSE}
   euler <- TRUE
   overlap <- list()  
-    if (ellipses  >2){
+  if (ellipses  >2){
     area   <- c()
     shared <- c()
     for (i in 1:3){
@@ -12,44 +12,41 @@ library(VennDiagram)
       else {j<-i+1}
       area[i]      <- length(elementsList[[i]])
       overlap[[i]] <- intersect(elementsList[[i]],elementsList[[j]])
-      shared[i]    <- length(overlap[[i]])
     }
-    #Intersect 1,2,3
-    overlap[[4]] <- intersect(intersect(elementsList[[1]],elementsList[[2]]),elementsList[[3]])
-    shared[4] <- length(overlap[[4]])
-    
-    if (ellipses <4){
-    venn.plot<-draw.triple.venn(area[1], area[2],area[3], 
-                                shared[1],shared[2],shared[3],shared[4],
-                                category = categories,scaled = scaleF,euler.d = euler,fill = colorValues, 
-                                lty = c(rep("solid",3)), cex = intLabSize, cat.cex = 2.5)}
-    else{
-      area[4] <- length(elementsList[[4]])
-      #Intersect 2,4
-      shared[5] <- length(intersect(elementsList[[2]],elementsList[[4]]))
-      #Intersect 3,4
-      shared[6] <- length(intersect(elementsList[[3]],elementsList[[4]]))
-      #Intersect 4,1
-      shared[7] <- length(intersect(elementsList[[1]],elementsList[[4]]))
-      #Intersect 1,2,4
-      shared[8] <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),elementsList[[4]]))
-      #Intersect 1,3,4
-      shared[9] <- length(intersect(intersect(elementsList[[1]],elementsList[[3]]),elementsList[[4]]))
-      #Intersect 2,3,4
-      shared[10] <- length(intersect(intersect(elementsList[[2]],elementsList[[3]]),elementsList[[4]]))
-      #Intersect 1,2,3,4
-      shared[11] <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),intersect(elementsList[[3]],elementsList[[4]]))) 
+    shared12 <- length(intersect(elementsList[[1]],elementsList[[2]]))
+    shared23 <- length(intersect(elementsList[[2]],elementsList[[3]]))
+    shared13 <- length(intersect(elementsList[[1]],elementsList[[3]]))
+    shared123 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),elementsList[[3]]))
+    if (ellipses >=4){
+        area[4] <- length(elementsList[[4]])
+        shared24 <- length(intersect(elementsList[[2]],elementsList[[4]]))
+        shared34 <- length(intersect(elementsList[[3]],elementsList[[4]]))
+        shared14 <- length(intersect(elementsList[[4]],elementsList[[1]]))
+        shared124 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),elementsList[[4]]))
+        shared134 <- length(intersect(intersect(elementsList[[1]],elementsList[[3]]),elementsList[[4]]))
+        shared234 <- length(intersect(intersect(elementsList[[2]],elementsList[[3]]),elementsList[[4]]))
+        shared1234 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),intersect(elementsList[[3]],elementsList[[4]]))) 
+    }
+    if (ellipses ==5){
+        area[5] <- length(elementsList[[5]])
+        shared15 <- length(intersect(elementsList[[1]],elementsList[[5]]))
+        shared25 <- length(intersect(elementsList[[2]],elementsList[[5]]))
+        shared35 <- length(intersect(elementsList[[3]],elementsList[[5]]))
+        shared45 <- length(intersect(elementsList[[4]],elementsList[[5]]))
+        shared125 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),elementsList[[5]]))
+        shared135 <- length(intersect(intersect(elementsList[[1]],elementsList[[3]]),elementsList[[5]]))
+        shared145 <- length(intersect(intersect(elementsList[[1]],elementsList[[4]]),elementsList[[5]]))
+        shared235 <- length(intersect(intersect(elementsList[[2]],elementsList[[3]]),elementsList[[5]]))
+        shared245 <- length(intersect(intersect(elementsList[[2]],elementsList[[4]]),elementsList[[5]]))
+        shared345 <- length(intersect(intersect(elementsList[[3]],elementsList[[4]]),elementsList[[5]]))
+        shared1235 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),intersect(elementsList[[3]],elementsList[[5]]))) 
+        shared1245 <- length(intersect(intersect(elementsList[[1]],elementsList[[2]]),intersect(elementsList[[4]],elementsList[[5]])))
+        shared1345 <- length(intersect(intersect(elementsList[[1]],elementsList[[3]]),intersect(elementsList[[4]],elementsList[[5]])))
+        shared2345 <- length(intersect(intersect(elementsList[[2]],elementsList[[3]]),intersect(elementsList[[4]],elementsList[[5]])))
+        shared12345 <-length(intersect(elementsList[[1]],intersect(intersect(elementsList[[2]],elementsList[[3]]),intersect(elementsList[[4]],elementsList[[5]]))))
+      }
+    }
 
-      venn.plot <- draw.quad.venn(area[1], area[2],area[3],area[4], 
-                                  shared[1],shared[3],shared[7],shared[2],shared[5],
-                                  shared[6],shared[4],shared[8],shared[9],shared[10],shared[11],
-                                  #n12, n13,n14, n23, n24,
-                                  #n34, n123, n124, n134, n234, n1234, 
-                                  category = categories,scaled = scaleF,fill = colorValues, 
-                                  lty = c(rep("solid",4)), cex = intLabSize, cat.cex = 2.5,euler.d = euler)
-      
-    }
-  }
   if (ellipses  == 2){
     area   <- c()
     shared <- c()
@@ -60,11 +57,33 @@ library(VennDiagram)
     overlap[[3]] <- intersect(elementsList[[1]],elementsList[[2]])
     #Intersect 1,2
     shared  <- length(overlap[[3]])
-    venn.plot<-draw.pairwise.venn(area[1], area[2],shared,
+    venn.plot<-draw.pairwise.venn(area[1], area[2],shared12,
                                 category = categories,scaled = scaleF,fill = colorValues, 
                                 lty = c(rep("solid",2)), cex = intLabSize, cat.cex = 2.5,euler.d = euler)
     
 
+  }
+  if (ellipses  == 3){
+      venn.plot<-draw.triple.venn(area[1], area[2],area[3], 
+                            shared12,shared23,shared13,shared123,
+                            category = categories,scaled = scaleF,euler.d = euler,fill = colorValues, 
+                            lty = c(rep("solid",3)), cex = intLabSize, cat.cex = 2.5)
+  }
+  if (ellipses  == 4){
+      venn.plot <- draw.quad.venn(area[1], area[2],area[3],area[4], 
+                            shared12,shared13,shared14,shared23,shared24,
+                            shared34,shared123,shared124,shared134,shared234,shared1234,
+                            category = categories,scaled = scaleF,fill = colorValues, 
+                            lty = c(rep("solid",4)), cex = intLabSize, cat.cex = 2.5,euler.d = euler)
+  }
+  if (ellipses  == 5){
+      venn.plot <- draw.quintuple.venn(area[1], area[2],area[3],area[4],area[5], 
+                                 shared12,shared13,shared14,shared15,shared23,shared24,shared25,
+                                 shared34,shared35,shared45,shared123,shared124,shared125,shared134,
+                                 shared135,shared145,shared234,shared235,shared245,shared345,shared1234,
+                                 shared1235,shared1245,shared1345,shared2345,shared12345,
+                                 category = categories,scaled = scaleF,fill = colorValues, 
+                                 lty = c(rep("solid",5)), cex = intLabSize, cat.cex = 2.5,euler.d = euler)
   }
   return(overlap)
 }

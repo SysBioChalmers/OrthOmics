9a99067d1933495b70e198614b417bbfe2c7801a
Ivan Domenzain <ivand@chalmers.se>
2020-03-11 17:20:37 +0100

    fix: simplify First_ID_self_self_duplicates script

--- Gene-Sorting-Example/First-ID-Self-Self-Duplicates.R
+++ Gene-Sorting-Example/First-ID-Self-Self-Duplicates.R
@@ -1,3 +1,4 @@
+First_ID_selfSelf_duplicates <- function(repoPath){
 #This script allows self-self Orthology searches to be quickly stripped down to a list of
 #genes and proteins that are predicted to be duplicates in an organism.
 
@@ -8,60 +9,50 @@
 #2) A list of genes that exist as 3 or more copies for other analyses
 #3) A list of genes to exclude from singleCopy Analysis called Duplicates_to_Exclude.csv
 
+library(splitstackshape)
+library(stringr)
+repoPath <- '/Users/ivand/Documents/GitHub/OrthOmics'
 #Load Necessary Data from the Self-Self orthology search
-setwd('/Users/doughty/Documents/GitHub/CHASSY_multiOmics_Analysis/Gene-Sorting-Example/S.cerevisiae_GeneSorting/GroupV')
-filename      <- 'Orthogroups.GeneCount.tsv'
-DuplicateOGs  <- read.delim(filename, header = TRUE, sep = "\t",stringsAsFactors=FALSE, na.strings = "NA")
-filename      <- 'Orthogroups.tsv'
-orthoGroups   <- read.delim(filename, header = TRUE, sep = "\t",stringsAsFactors=FALSE, na.strings = "NA")
-endCol        <- ncol(orthoGroups)
-newOGlist     <- c()
-group_number  <- 'Duplicates_to_Exclude.csv' #Add the group number for the analysis
-filename   <- '/Users/doughty/Documents/GitHub/CHASSY_multiOmics_Analysis/Gene-Sorting-Example/S.cerevisiae_GeneSorting/s288c_gene_prot.csv'
-gene_2_prot <- read.csv(filename, header = TRUE)
-
+groupingPath <- paste(repoPath,'/Gene-Sorting-Example/Scerevisiae_GeneSorting',sep='')
+setwd(groupingPath)
+filename     <- paste(groupingPath,'/GroupV/Orthogroups.GeneCount.tsv',sep='')
+DuplicateOGs <- read.delim(filename, header = TRUE, sep = "\t",stringsAsFactors=FALSE, na.strings = "NA")
+filename     <- paste(groupingPath,'/GroupV/Orthogroups.tsv',sep='')
+orthoGroups  <- read.delim(filename, header = TRUE, sep = "\t",stringsAsFactors=FALSE, na.strings = "NA")
+endCol       <- ncol(orthoGroups)
+group_number <- 'Duplicates_to_Exclude.csv' #Add the group number for the analysis
+filename     <- paste(groupingPath,'/s288c_gene_prot.csv',sep='')
+gene_2_prot  <- read.csv(filename, header = TRUE)
+newOGlist    <- c()
 #Keep only self-self duplicates (i.e. 2+ copy genes)
-DuplicateOGs <- DuplicateOGs[apply(DuplicateOGs[c(2)],1,function(z) any(z!=0)),] #Organism of interest in column 2 - Remove orthogroups with 0 values for the organism of interest (these are orthologs between the other organisms but not the query)
-DuplicateOGs <- DuplicateOGs[apply(DuplicateOGs[c(2)],1,function(z) any(z>=2)),] # grab duplicates from self-self orthology search
-
+rownames(DuplicateOGs) <- DuplicateOGs[,1]
+DuplicateOGs           <- DuplicateOGs[,2:endCol]
+#Organism of interest in column 1 - Remove orthogroups with 0 values for the organism 
+#of interest (these are orthologs between the other organisms but not the query)
+DuplicateOGs <- DuplicateOGs[apply(DuplicateOGs[c(1)],1,function(z) any(z!=0)),] 
+# grab duplicates from self-self orthology search
+DuplicateOGs <- DuplicateOGs[apply(DuplicateOGs[c(1)],1,function(z) any(z>=2)),] 
 #Map each  Gene/protein to orthoGroups data frame
 for (i in 1:nrow(DuplicateOGs)){
-  OGid    <- DuplicateOGs[i,1] 
-  index   <- which(orthoGroups[,1]==OGid)
-  rowData <- orthoGroups[index,2:endCol]
-  newRow  <- OGid
-  
-  
-#Split  protein ID's for each organism
-  for (j in 1:(endCol-1)){
-    cellText  <- rowData[[j]]
-
-    newRow    <- cbind(newRow,cellText)
-  }
+  OGid      <- rownames(DuplicateOGs)[i] 
+  index     <- which(orthoGroups[,1]==OGid)
+  rowData   <- orthoGroups[index,2:endCol]
+  newRow    <- orthoGroups[index,]
   newOGlist <- rbind(newOGlist,newRow)
 }
 colnames(newOGlist) <- colnames(orthoGroups)
-return(newOGlist)
-
-
-
+#return(newOGlist)
 newOGlistP <- newOGlist [,c(1:2)]
 newOGlistP2 <- cSplit(newOGlistP, 'A_s288c', sep=",", type.convert=FALSE)
-
 #1) Create a list of duplicates for sorting later on in the format that will match orthofinder outputs
 #Keep the protein IDs inside of their respective orthogroups for future sorting
 MultiCopyGroups <- lapply(newOGlistP2, word, 1)
-
 MultiCopyGroups <- as.data.frame(MultiCopyGroups)
 rownames(MultiCopyGroups) <- MultiCopyGroups$Orthogroup
 MultiCopyGroups <- MultiCopyGroups[,-1]
-
 write.table(MultiCopyGroups, file="Duplications_Groups_For_Sorting.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = ", ", na="")
-
 #2) Create a list of genes that have 3+copies in the organism of interest - This helps for downstream analyses
-
 Three_Plus_Copy_Genes <- MultiCopyGroups[complete.cases(MultiCopyGroups[ ,3 ]), ]
-
 Three_Plus_Copy_Genes <- data.frame(matrix(unlist(Three_Plus_Copy_Genes), byrow=T), stringsAsFactors=FALSE)
 namevector <- c("3+CopyGenes")
 Three_Plus_Copy_Genes[, namevector] <- "3+CopyGenes"
@@ -71,7 +62,6 @@ Three_Plus_Copy_Genes <- Three_Plus_Copy_Genes[complete.cases(Three_Plus_Copy_Ge
 Three_Plus_Copy_Genes <- merge(Three_Plus_Copy_Genes, gene_2_prot, by.x = "protein", by.y = "protein", all.x= TRUE)
 Three_Plus_Copy_Genes <- Three_Plus_Copy_Genes[,c(1,3,2)]
 write.table(Three_Plus_Copy_Genes, file="3+Copy_Genes.csv", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = ",")
-
 #3) Create a list of duplicated genes to exclude from singlegene sorting
 #Pull all uniprot IDs into a linear list                    
 newOGlistQ <- newOGlist [,c(2)]
@@ -81,7 +71,6 @@ num.el <- sapply(test, length)
 newOGsimple <- cbind(unlist(test), rep(1:length(test), num.el))
 colnames(newOGsimple) <- c("protein","sortingGroup")
 newOGsimple <- as.data.frame(newOGsimple)
-library(splitstackshape)
 newOGsimple2 <- cSplit(newOGsimple, 'protein', sep=" ", type.convert=FALSE)
 newOGsimple2$sortingGroup <- gsub('1', 'Self-Self-Duplicate', newOGsimple$sortingGroup)
 newOGsimple2 <- newOGsimple2[,c(1,2)]
@@ -92,5 +81,5 @@ m <- merge(newOGsimple2, gene_2_prot, by.x = "protein", by.y = "protein", all.x=
 m  <- m [,c(1,4,2)]
 
 write.csv(m, file = group_number, row.names = FALSE)
-
+}
 
